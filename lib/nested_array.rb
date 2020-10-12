f08a08226fa807bc03c25c0cc63fdc87bb8040b7
Josh Castro <castrojoshua805@gmail.com>
2020-10-12 20:28:53 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -41,6 +41,6 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
-  matrix[row][column] == new_value
-  p matrix
+  new_matrix = matrix[row][column] == new_value
+  p new_matrix
 end
