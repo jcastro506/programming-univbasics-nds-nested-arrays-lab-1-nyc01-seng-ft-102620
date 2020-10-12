506674c4ccce6b52ca9ebdfeeb91019bc42a74ee
Josh Castro <castrojoshua805@gmail.com>
2020-10-12 20:23:14 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,18 +17,32 @@ CONVENTIONAL_PRODUCE = [
 ]
 
 def assembled_matrix
-  matrix = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
+  new_array = [
+    CONVENTIONAL_PRODUCE,
+    ORGANIC_PRODUCE,
+    ]
 end
 
+def array_literal_matrix
+  # Using Array literal syntax only, build a nested array that uses the data in
+  # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
+  # alphabetically by the first character.
+end 
+
 def sorted_matrix
-  new_array = [CONVENTIONAL_PRODUCE.sort, ORGANIC_PRODUCE.sort]
+  sorted_array = [
+    CONVENTIONAL_PRODUCE.sort,
+    ORGANIC_PRODUCE.sort,
+    ]
 end
 
 def matrix_lookup(matrix, row, column)
-  matrix[row][column]
+  p matrix[row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
-  matrix[row][column] = new_value
-  return matrix
+  # Update the matrix location at row and column to have the value of new_value
+  # Given any matrix (array of arrays), a row index and a column index, 
+  # Update the matrix location at that row and column to have the value of new_value
+  # Return the updated matrix
 end
